d10c2a8378e8a182c480d064707e0d4bcb79b383
Aryeh Sanders <aryehsanders@gmail.com>
2016-07-28 12:12:12 +0300

    Stop showing spinner if the request failed

--- dTiberias/Controllers/Directives/InProgressDirective.js
+++ dTiberias/Controllers/Directives/InProgressDirective.js
@@ -4,6 +4,7 @@
         templateUrl: 'partials/templates/InProgressTemplate.html',
         controller: ['$scope', 'InProgressService', function ($scope, InProgressService) {
             $scope.showInProcess = InProgressService.isReady != 1;
+            $scope.showInProcessError = InProgressService.errorData;
             $scope.$on('isReady_Updated', function () {
                 $scope.showInProcess = InProgressService.isReady != 1;
             });
--- dTiberias/Controllers/Services/InProgressService.js
+++ dTiberias/Controllers/Services/InProgressService.js
@@ -2,9 +2,16 @@
     var root = {};
 
     root.isReady = 1;
+    root.errorData = { error: false, errorText: "" };
     root.updateIsReady = function (value) {
         this.isReady = value;
         $rootScope.$broadcast("isReady_Updated");
+    }
+
+    root.setError = function (errorText) {
+        root.updateIsReady(1);
+        root.errorData.error = true;
+        root.errorData.errorText = errorText == "" ? "Unknown Server Error" : errorText;
     }
 
     return root;

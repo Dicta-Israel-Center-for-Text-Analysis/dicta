678b6bccc29a30f4acfa9f92dfb63ff9d06e4141
Aryeh Sanders <aryehsanders@gmail.com>
2016-07-31 16:40:44 +0300

    rearrange SelectClassService

--- dTiberias/Controllers/Services/Class/SelectClassService.js
+++ dTiberias/Controllers/Services/Class/SelectClassService.js
@@ -1,34 +1,34 @@
 ﻿jTextMinerApp.factory('SelectClassService', function ($rootScope) {
-    var service = {};
-    
-    service.Select_NumberOfChapters = 0;
+    var service = {
+        Select_NumberOfChapters: 0,
+        Select_NumberOfWords: 0,
+        Select_ChunkMode: 'DoNotChunk',
+        Select_MinimumChunkSize: 250,
+        Select_ChunkSize: 0,
+        lastSelectedRootKeys: [],
+        lastTestSetSelectedRootKeys: []
+    };
+
+    // broadcast any changes, except for lastTestSetSelectedRootKeys
     $rootScope.$watch('Select_NumberOfChapters', function () {
         $rootScope.$broadcast("Select_NumberOfChaptersUpdated");
     });
-    service.Select_NumberOfWords = 0;
     $rootScope.$watch('Select_NumberOfWords', function () {
         $rootScope.$broadcast("Select_NumberOfWordsUpdated");
     });
-    service.Select_ChunkMode = 'DoNotChunk';
     $rootScope.$watch('Select_ChunkMode', function () {
         $rootScope.$broadcast("Select_ChunkModeUpdated");
     });
-    service.Select_MinimumChunkSize = 250;
     $rootScope.$watch('Select_MinimumChunkSize', function () {
         $rootScope.$broadcast("Select_MinimumChunkSizeUpdated");
     });
-    service.Select_ChunkSize = 0;
     $rootScope.$watch('Select_ChunkSize', function () {
         $rootScope.$broadcast("Select_ChunkSizeUpdated");
     });
-
-    service.lastSelectedRootKeys = [];
     $rootScope.$on('lastSelectedRootKeys', function (event, data) {
         service.lastSelectedRootKeys = data;
         $rootScope.$broadcast("lastSelectedRootKeysUpdated");
     });
-
-    service.lastTestSetSelectedRootKeys = [];
     
     return service;
 });

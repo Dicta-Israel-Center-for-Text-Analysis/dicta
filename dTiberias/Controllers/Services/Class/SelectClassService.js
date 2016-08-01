7a0b5391f72a9e0364582c5fd3d386f9dd92d90d
Aryeh Sanders <aryehsanders@gmail.com>
2016-08-01 18:02:56 +0300

    correct the calculation of the longest common prefix of the selected keys

--- dTiberias/Controllers/Services/Class/SelectClassService.js
+++ dTiberias/Controllers/Services/Class/SelectClassService.js
@@ -33,15 +33,15 @@
 
     service.setTestSetRootKeys = function(rootKeys) {
         service.lastTestSetSelectedRootKeys = rootKeys;
-        var keyCopy = rootKeys.slice();
-        var oneKey = keyCopy.shift();
-        var commonKeySegments = oneKey.split('/');
-        for (oneKey in keyCopy) {
-            var keySegments = oneKey.split('/');
-            for (var i = 0; i < commonKeySegments.length; i++) {
-                if (i > keySegments.length + 1 || commonKeySegments[i] != keySegments[i]) {
-                    keySegments.splice(i);
-                    break;
+        var keyCopy = rootKeys.slice();             // copy the list of keys
+        var oneKey = keyCopy.shift();               // take the first key
+        var commonKeySegments = oneKey.split('/');  // split on '/' into an array
+        for (oneKey in keyCopy) {                   // check each key to see how much it has in common with the others
+            var keySegments = oneKey.split('/');    // split the key we're looking at
+            for (var i = 0; i < commonKeySegments.length; i++) {                                // for each common key segment
+                if (i == keySegments.length || commonKeySegments[i] != keySegments[i]) {        // if the key we're looking at doesn't have a segment at this position or if the segment at this position doesn't match, we've gone too far
+                    commonKeySegments.splice(i);                                                // so trim the list of common segments here
+                    break;                                                                      // and check the next key
                 }
             }
         }

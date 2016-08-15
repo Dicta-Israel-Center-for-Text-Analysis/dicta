1bc93722d87c84684c756b06cf410782ead82ad3
Aryeh Sanders <aryehsanders@gmail.com>
2016-08-15 17:31:39 +0300

    start work for reset on New Experiment

--- dTiberias/Controllers/Services/Class/BrowseClassService.js
+++ dTiberias/Controllers/Services/Class/BrowseClassService.js
@@ -1,8 +1,21 @@
 ﻿jTextMinerApp.factory('BrowseClassService', function ($rootScope, InProgressService) {
-    var service = {};
+    var initialServiceValues = {
+        countWordsForUploadedZipFile: [],
+        countWordsForUploadedTxtFile: [],
+        Browse_DoNotChunk_ChunkSize: 0,
+        Browse_AppendAndChunk_ChunkSize: 0,
+        Browse_ChunkBigFiles_ChunkSize: 0,
+        Browse_NumberOfFiles: 0,
+        Browse_NumberOfWords: 0,
+        Browse_ChunkMode: 'DoNotChunk',
+        zipFile: '',
+        Browse_FileName: '', // $scope.zipFile.name
+        Browse_MinimumChunkSize: 250,
+        LastClassTotalNumberOfWords: 0
+    };
+    var service = angular.copy(initialServiceValues);
 
     // class dialog parameters
-    service.countWordsForUploadedZipFile = [];
     service.updateCountWordsForUploadedZipFile = function (value) {
         this.countWordsForUploadedZipFile = value;
         InProgressService.updateIsReady(1);
@@ -10,7 +23,6 @@
     }
     // end class dialog parameters
     // txt class dialog parameters
-    service.countWordsForUploadedTxtFile = [];
     service.updateCountWordsForUploadedTxtFile = function (value) {
         this.countWordsForUploadedTxtFile = value;
         InProgressService.updateIsReady(1);
@@ -18,68 +30,60 @@
     }
     // end txt class dialog parameters
 
-    service.Browse_DoNotChunk_ChunkSize = 0;
     service.updateBrowse_DoNotChunk_ChunkSize = function (value) {
         this.Browse_DoNotChunk_ChunkSize = value;
         $rootScope.$broadcast("Browse_DoNotChunk_ChunkSizeUpdated");
     }
 
-    service.Browse_AppendAndChunk_ChunkSize = 0;
     service.updateBrowse_AppendAndChunk_ChunkSize = function (value) {
         this.Browse_AppendAndChunk_ChunkSize = value;
         $rootScope.$broadcast("Browse_AppendAndChunk_ChunkSizeUpdated");
     }
     
-    service.Browse_ChunkBigFiles_ChunkSize = 0;
     service.updateBrowse_ChunkBigFiles_ChunkSize = function (value) {
         this.Browse_ChunkBigFiles_ChunkSize = value;
         $rootScope.$broadcast("Browse_ChunkBigFiles_ChunkSizeUpdated");
     }
     
-    service.Browse_NumberOfFiles = service.countWordsForUploadedZipFile.length;
     service.updateBrowse_NumberOfFiles = function (value) {
         this.Browse_NumberOfFiles = value;
         $rootScope.$broadcast("Browse_NumberOfFilesUpdated");
     }
     
-    service.Browse_NumberOfWords = 0;
     service.updateBrowse_NumberOfWords = function (value) {
         this.Browse_NumberOfWords = value;
         $rootScope.$broadcast("Browse_NumberOfWordsUpdated");
     }
     
-    service.Browse_ChunkMode = 'DoNotChunk';
     service.updateBrowse_ChunkMode = function (value) {
         this.Browse_ChunkMode = value;
         $rootScope.$broadcast("Browse_ChunkModeUpdated");
     }
     
-    service.zipFile = '';
-    
-    service.Browse_FileName = ''; // $scope.zipFile.name
     service.updateBrowse_FileName = function (value) {
         this.Browse_FileName = value;
         $rootScope.$broadcast("Browse_FileNameUpdated");
     }
     
-    service.Browse_ChunkMode = 'DoNotChunk';
-    service.updateBrowse_ChunkMode = function (value) {
-        this.Browse_ChunkMode = value;
-        $rootScope.$broadcast("Browse_ChunkModeUpdated");
-    }
-    
-    service.Browse_MinimumChunkSize = 250;
     service.updateBrowse_MinimumChunkSize = function (value) {
         this.Browse_MinimumChunkSize = value;
         $rootScope.$broadcast("Browse_MinimumChunkSizeUpdated");
     }
 
-    service.LastClassTotalNumberOfWords = 0;
     service.updateLastClassTotalNumberOfWordsValue = function (value) {
         this.LastClassTotalNumberOfWords = value;
         $rootScope.$broadcast("LastClassTotalNumberOfWordsValueUpdated");
     }
     
+    service.reset = function () {
+        var newCopy = angular.copy(initialServiceValues);
+        for (var prop in newCopy)
+        {
+            service[prop] = newCopy[prop];
+        }
+    };
+
+    $rootScope.$on('NewExperiment', service.reset);
 
     return service;
 });

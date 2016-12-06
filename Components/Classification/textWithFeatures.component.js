78b3869830e713126883b5a651adbc4715475a71
Aryeh Sanders <aryehsanders@gmail.com>
2016-12-06 14:33:48 +0200

    start breaking out a separate text control

---
+++ Components/Classification/textWithFeatures.component.js
@@ -0,0 +1,14 @@
+﻿jTextMinerApp.component('textWithFeatures', {
+    bindings: {
+        testFile: '<',
+        index: '<',
+        classifiedColor: '<',
+        features: '<'
+    },
+    templateUrl: 'Components/Classification/textWithFeatures.component.html',
+    controller: ['$scope', '$sce', function ($scope, $sce) {
+        $scope.convert2TrustAsHtml = function (text) {
+            return $sce.trustAsHtml(text);
+        };
+    }]
+});

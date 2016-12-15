51178f14c2d37362bcea3f9453a3f85ca266bb19
Aryeh Sanders <aryehsanders@gmail.com>
2016-12-15 17:48:07 +0200

    fix karma.conf.js

--- karma.conf.js
+++ karma.conf.js
@@ -3,17 +3,22 @@ module.exports = function(config) {
   config.set({
 
     files: [
-	  'js/Common/jquery.min.js',
-      'js/Common/jquery.cookie.js',
-	  'node_modules/angular/angular.min.js',
+	  'node_modules/jquery/dist/jquery.min.js',
+      'node_modules/angular/angular.min.js',
+      'node_modules/angular-animate/angular-animate.min.js',
 	  'node_modules/angular-resource/angular-resource.min.js',
 	  'node_modules/angular-ui-router/release/angular-ui-router.min.js',
 	  'node_modules/angular-mocks/angular-mocks.js',
+      'node_modules/angular-ui-bootstrap/dist/ui-bootstrap-tpls.js',
+      'node_modules/angular-ui-indeterminate/dist/indeterminate.min.js',
+      'node_modules/angular-ui-router/release/angular-ui-router.min.js',
+      'node_modules/ng-dialog/js/ngDialog.min.js',
+      'node_modules/hopscotch/dist/js/hopscotch.min.js',
 	  'Controllers/*.js',
       'Controllers/**/*.js',
       'Components/**/*.js',
 	  'Services/**/*.js',
-	  'js/**/!(jquery)*.js'
+	  'js/**/*.js'
     ],
 
     autoWatch: true,

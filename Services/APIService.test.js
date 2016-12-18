3bc8ab0131e5a21db86b14451515ee53c01a61ef
Aryeh Sanders <aryehsanders@gmail.com>
2016-12-18 21:05:58 +0200

    sanity tests for APIService

---
+++ Services/APIService.test.js
@@ -0,0 +1,43 @@
+describe('Service: JTextMinerApp.APIService', function () {
+
+    // load the service's module
+    beforeEach(module('JTextMinerApp'));
+
+    // instantiate service
+    var APIService;
+    var httpBackend;
+
+    //update the injection
+    beforeEach(inject(function (_APIService_, $httpBackend) {
+        APIService = _APIService_;
+        httpBackend = $httpBackend;
+    }));
+
+    it('should provide a resource in response to a legacy call to APIService', function () {
+        httpBackend.expectPOST(/JTextMinerAPI\/CheckUserLogin/).respond({userLogin: 'eden'});
+        APIService.apiRun({ crud: 'CheckUserLogin' }, { userLogin: 'eden' })
+            .$promise.then(function(response) {
+                expect(response.toJSON()).toEqual({userLogin: 'eden'});
+            });
+        httpBackend.flush();
+    });
+
+    it('should make an HTTP POST when the call method is used', function () {
+        httpBackend.expectPOST(/JTextMinerAPI\/CheckUserLogin/).respond({userLogin: 'eden'});
+        APIService.call('JTextMinerAPI/CheckUserLogin', { userLogin: 'eden' })
+            .then(function(response) {
+            expect(response.data).toEqual({userLogin: 'eden'});
+        });
+        httpBackend.flush();
+    });
+
+    it('should make an HTTP POST to the parallels server when the callParallels method is used', function () {
+        httpBackend.expectPOST("http://www.dictaparallelsserver.com/api/parallels").respond({testData: 'test'});
+        APIService.callParallels({ text: 'testText' })
+            .then(function(response) {
+            expect(response.data).toEqual({testData: 'test'});
+        });
+        httpBackend.flush();
+    });
+
+});

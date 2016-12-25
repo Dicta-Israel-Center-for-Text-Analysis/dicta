7135a1e2660785b484356658ce9632e29ff7d341
Aryeh Sanders <aryehsanders@gmail.com>
2016-12-25 16:42:50 +0200

    get rid of scope in selectOnlineCorpus.component

--- Components/Shared/TextSelection/selectOnlineCorpus.component.js
+++ Components/Shared/TextSelection/selectOnlineCorpus.component.js
@@ -1,39 +1,40 @@
 ﻿jTextMinerApp.component('selectOnlineCorpus', {
         templateUrl: 'Components/Shared/TextSelection/selectOnlineCorpus.component.html',
-        controller: ['$scope', '$rootScope', 'TreeService', 'SelectClassService', function ($scope, $rootScope, TreeService, SelectClassService) {
+        controller: ['TreeService', 'SelectClassService', function (TreeService, SelectClassService) {
+            var ctrl = this;
             function initBreadCrumbs() {
-                $scope.breadCrumbs = ['All Collections'];
+                ctrl.breadCrumbs = ['All Collections'];
             }
             initBreadCrumbs();
 
             TreeService.readyPromise.then(function() {
-                $scope.corpusTree = TreeService.corpusTree;
-                $scope.treeNode = $scope.corpusTree;
+                ctrl.corpusTree = TreeService.corpusTree;
+                ctrl.treeNode = ctrl.corpusTree;
                 initCurrentLevel(null);
             });
 
             // based on http://stackoverflow.com/questions/14514461/how-to-bind-to-list-of-checkbox-values-with-angularjs
             // list selected nodes by key
-            $scope.selectedNodes = SelectClassService.lastSelectedRootKeys.map(function(key){return key.substring("/Dicta Corpus/".length)});
+            ctrl.selectedNodes = SelectClassService.lastSelectedRootKeys.map(function(key){return key.substring("/Dicta Corpus/".length)});
             recalculatePartials();
 
             function initCurrentLevel(parentNode) {
-                for (var i = 0; i < $scope.treeNode.length; i++) {
+                for (var i = 0; i < ctrl.treeNode.length; i++) {
                     // add links back to the parent, so we can update selections
-                    $scope.treeNode[i]['parent'] = parentNode;
-                    TreeService.keyToNode[$scope.treeNode[i]['key']] = $scope.treeNode[i];
+                    ctrl.treeNode[i]['parent'] = parentNode;
+                    TreeService.keyToNode[ctrl.treeNode[i]['key']] = ctrl.treeNode[i];
                 }
             }
 
             // end of tree setup; helper functions come next
 
-            $scope.expandNode = function (itemTitle) {
-                var currentNode = $scope.treeNode;
+            ctrl.expandNode = function (itemTitle) {
+                var currentNode = ctrl.treeNode;
                 for (var i=0; i < currentNode.length; i++) {
                     if (currentNode[i]['title'] == itemTitle) {
-                        $scope.treeNode = currentNode[i]['children'];
+                        ctrl.treeNode = currentNode[i]['children'];
                         initCurrentLevel(currentNode[i]);
-                        $scope.breadCrumbs.push(itemTitle);
+                        ctrl.breadCrumbs.push(itemTitle);
                         break;
                     }
                 }
@@ -52,83 +53,83 @@
             }
 
             function recalculatePartials() {
-                $scope.partialNodes = [];
-                for (var i=0;i< $scope.selectedNodes.length; i++) {
-                    var parent = TreeService.keyToNode[$scope.selectedNodes[i]]['parent'];
+                ctrl.partialNodes = [];
+                for (var i=0;i< ctrl.selectedNodes.length; i++) {
+                    var parent = TreeService.keyToNode[ctrl.selectedNodes[i]]['parent'];
                     while (parent != null) {
-                        if ($scope.partialNodes.indexOf(parent['key']) == -1)
-                            $scope.partialNodes.push(parent['key']);
+                        if (ctrl.partialNodes.indexOf(parent['key']) == -1)
+                            ctrl.partialNodes.push(parent['key']);
                         parent = parent['parent'];
                     }
                 }
             }
 
-            $scope.isNodeSelected = function isNodeSelected(node) {
+            ctrl.isNodeSelected = function isNodeSelected(node) {
                 var parent = TreeService.keyToNode[node]['parent'];
-                return $scope.selectedNodes.indexOf(node) > -1 || (parent != null && isNodeSelected(parent['key']));
+                return ctrl.selectedNodes.indexOf(node) > -1 || (parent != null && isNodeSelected(parent['key']));
             };
 
-            $scope.isNodePartial = function isNodePartial(node) {
-                return $scope.partialNodes.indexOf(node) > -1;
+            ctrl.isNodePartial = function isNodePartial(node) {
+                return ctrl.partialNodes.indexOf(node) > -1;
             };
 
             // toggle selection for a given node by key
-            $scope.toggleSelection = function toggleSelection(itemKey) {
+            ctrl.toggleSelection = function toggleSelection(itemKey) {
                 // is currently selected, so this deselects
-                if ($scope.isNodeSelected(itemKey)) {
+                if (ctrl.isNodeSelected(itemKey)) {
                     // currently selected itself?
-                    var idx = $scope.selectedNodes.indexOf(itemKey);
+                    var idx = ctrl.selectedNodes.indexOf(itemKey);
                     if (idx > -1) {
                         // yes, so remove it.
-                        $scope.selectedNodes.splice(idx, 1);
+                        ctrl.selectedNodes.splice(idx, 1);
                     }
                     else {
                         // select siblings and deselect ancestor
                         var currentNode = TreeService.keyToNode[itemKey];
-                        while ((idx = $scope.selectedNodes.indexOf(currentNode['key'])) == -1) {
+                        while ((idx = ctrl.selectedNodes.indexOf(currentNode['key'])) == -1) {
                             var children = currentNode['parent']['children'];
                             for (var i = 0; i < children.length; i++)
                                 if (children[i] != currentNode)
-                                    $scope.selectedNodes.push(children[i]['key']);
+                                    ctrl.selectedNodes.push(children[i]['key']);
                             currentNode = currentNode['parent'];
                         }
-                        $scope.selectedNodes.splice(idx, 1);
+                        ctrl.selectedNodes.splice(idx, 1);
                     }
                 }
                 // is newly selected
                 else {
                     // select this
-                    $scope.selectedNodes.push(itemKey);
+                    ctrl.selectedNodes.push(itemKey);
                     // check if all siblings are selected
                     var parent = TreeService.keyToNode[itemKey]['parent'];
                     if (parent != null) {
                         var sibCount = 0;
                         var siblings = parent['children'];
                         for (var i = 0; i < siblings.length; i++)
-                            if ($scope.selectedNodes.indexOf(siblings[i]['key']) > -1)
+                            if (ctrl.selectedNodes.indexOf(siblings[i]['key']) > -1)
                                 sibCount++;
                         // if so, select the parent
                         if (sibCount == siblings.length)
-                            $scope.toggleSelection(parent['key']);
+                            ctrl.toggleSelection(parent['key']);
 
                     }
                 }
 
                 // remove any descendants already selected
-                for (var i = $scope.selectedNodes.length - 1; i>=0; i--)
-                    removeIfAncestor($scope.selectedNodes, i, TreeService.keyToNode[itemKey]);
+                for (var i = ctrl.selectedNodes.length - 1; i>=0; i--)
+                    removeIfAncestor(ctrl.selectedNodes, i, TreeService.keyToNode[itemKey]);
 
                 recalculatePartials();
 
                 SelectClassService.lastSelectedRootKeys = ctrl.selectedNodes.map(function(key){return "/Dicta Corpus/" + key;});
             };
 
-            $scope.selectCrumb = function (crumbNumber) {
-                var oldCrumbs = $scope.breadCrumbs;
+            ctrl.selectCrumb = function (crumbNumber) {
+                var oldCrumbs = ctrl.breadCrumbs;
                 initBreadCrumbs();
-                $scope.treeNode = $scope.corpusTree;
+                ctrl.treeNode = ctrl.corpusTree;
                 for (var i=1; i <= crumbNumber; i++) {
-                    $scope.expandNode(oldCrumbs[i]);
+                    ctrl.expandNode(oldCrumbs[i]);
                 }
 
             };

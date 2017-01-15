5663322df1acdf30e54f34c08cbfc72d2ab114da
Aryeh Sanders <aryehsanders@gmail.com>
2017-01-15 15:46:11 +0200

    start process of allowing HTTPS

--- Services/APIService.js
+++ Services/APIService.js
@@ -7,9 +7,10 @@
  *
  * */
 angular.module('JTextMinerApp')
-        .factory("APIService", function ($resource, $http) {
-    var baseUrl = "http://localhost:8080/NewWebSite/api";
-    //baseUrl = "http://ec2-35-156-213-159.eu-central-1.compute.amazonaws.com/WebServiceJTextMinerNewRoot8/api";
+        .factory("APIService", function ($resource, $http, $location) {
+    var baseUrl = $location.protocol() + "://localhost:8080/NewWebSite/api";
+    //baseUrl = $location.protocol() + "://dev.dicta.org.il/WebServiceJTextMinerDev/api";
+    baseUrl = $location.protocol() + "://ec2-35-156-213-159.eu-central-1.compute.amazonaws.com/WebServiceJTextMinerNewRoot8/api";
     var url = baseUrl + "/JTextMinerAPI";
 
     var APIService = $resource(url + "/:crud/:secondParam",

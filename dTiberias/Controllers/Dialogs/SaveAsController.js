e911fcd752e83fa9bcf409a45dd66de92154f1b5
Aryeh Sanders <aryehsanders@gmail.com>
2017-02-27 15:33:57 +0200

    remove dTiberias, tTiberias, and so on; still available in git history

--- dTiberias/Controllers/Dialogs/SaveAsController.js
+++
@@ -1,9 +0,0 @@
-﻿// create the controller and inject Angular's $scope
-jTextMinerApp.controller('SaveAsController', function ($scope, ngDialog, ExperimentService) {
-    $scope.expName = ExperimentService.ExperimentName;
-    $scope.Save = function () {
-        ExperimentService.updateExperimentName($scope.expName);
-        ExperimentService.SaveExperiment();
-        $scope.confirm();
-    }
-});

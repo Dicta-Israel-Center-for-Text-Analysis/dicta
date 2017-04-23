b42b0308cda16168609e70704f635d39b55be7fd
Aryeh Sanders <aryehsanders@gmail.com>
2017-04-23 14:26:58 +0300

    autocomplete for search

--- Components/Search/search.component.js
+++ Components/Search/search.component.js
@@ -18,9 +18,18 @@ jTextMinerApp.component('search',
                 $http.post("http://dev.dicta.org.il/essearch/",{
                     "query": {
                         "multi_match": {
-                            "fields": ["_all", "parsed_text.x", "parsed_text.y"],
-                            "query": ctrl.searchTerm
+                            "fields": ["parsed_text*"],
+                            "query": ctrl.searchTerm,
+                            "tie_breaker": 0.001,
+                            "minimum_should_match": "3<90%"
                         }
+                    },
+                    "highlight": {
+                        "pre_tags" : ["<mark>"],
+                        "post_tags" : ["</mark>"],
+                        "fields": {"parsed_text.y": {
+                            fragment_size: 10000
+                        }}
                     },
                     "from": offset,
                     "size": ctrl.RESULTS_AT_A_TIME
@@ -33,13 +42,42 @@ jTextMinerApp.component('search',
                     })
             };
 
+            ctrl.setSearchTerm = function (selected) {
+                ctrl.searchTerm = selected.title;
+                ctrl.search();
+            };
+
+            ctrl.inputChanged = function (searchTerm) {
+                ctrl.searchTerm = searchTerm;
+            };
+
             ctrl.highlight = function (text) {
+                if (text.highlight && text.highlight['parsed_text.y'])
+                    return text.highlight['parsed_text.y'].join('...');
                 const re = new RegExp("(" + ctrl.searchTerm.replace(/ /g,'|') + ")", "g");
-                return text.replace(re, "<mark>$1</mark>")
+                return text._source.parsed_text.replace(re, "<mark>$1</mark>")
             };
 
             ctrl.nextResults = function () {
                 ctrl.search(ctrl.offset + ctrl.RESULTS_AT_A_TIME)
+            }
+
+            ctrl.runSuggest = function(userInputString, timeoutPromise) {
+                return $http.post("http://dev.dicta.org.il/essearch/",
+                    {
+                        "suggest": {
+                            "my-suggestion": {
+                                "prefix": userInputString,
+                                "term": {
+                                    "field": "parsed_text.y"
+                                }
+                            }
+                        }
+                    },
+                    {timeout: timeoutPromise})
+                    .then(response =>
+                        response.data.suggest["my-suggestion"]["0"].options
+                    );
             }
         }]
 }); 

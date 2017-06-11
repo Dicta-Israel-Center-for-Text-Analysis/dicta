bf4b90f26495271dfd09234d293e4962937227f1
Aryeh Sanders <aryehsanders@gmail.com>
2017-06-11 16:14:44 +0300

    accept parameters sortByScore:true and lexeme:דוד=/ in the search box

--- Components/Search/search.service.js
+++ Components/Search/search.service.js
@@ -11,15 +11,28 @@ angular.module('JTextMinerApp')
                 if (!offset)
                     offset = 0;
                 service.searching = true;
+                const queryParamRegex = /(\S+:\S+)/g;
+                const baseQueryString = service.query.replace(queryParamRegex, '');
+                const queryParamsArray = service.query.match(queryParamRegex);
+                let queryParams = {};
+                if (queryParamsArray) {
+                    queryParamsArray.forEach(
+                        match => {
+                            let [paramName, paramValue] = match.split(':');
+                            queryParams[paramName] = paramValue;
+                        }
+                    )
+                }
                 const baseQuery = {
                     "multi_match": {
                         "fields": ["parsed_text*"],
-                        "query": service.query,
+                        "query": baseQueryString,
                         "tie_breaker": 0.001,
                         "minimum_should_match": "3<80%"
                     }
                 };
-                return $http.post("http://dev.dicta.org.il/essearch/", {
+
+                const fullQuery = {
                     "query": {
                         "bool": {
                             "should": baseQuery,
@@ -42,8 +55,13 @@ angular.module('JTextMinerApp')
                     },
                     "from": offset,
                     "size": service.RESULTS_AT_A_TIME,
-                    "sort": { "corpus_order_path": { "order": "asc" }}
-                })
+                    "track_scores": true
+                };
+                if (!queryParams['sortByScore'])
+                    fullQuery["sort"] = { "corpus_order_path": { "order": "asc" }};
+                if (queryParams['lexeme'])
+                    fullQuery.query.bool["filter"] = { "match": { "lemmas*": queryParams['lexeme'] }};
+                return $http.post("http://dev.dicta.org.il/essearch/", fullQuery)
                     .then(function (response) {
                         service.searchResults = response.data.hits.hits;
                         service.searchResponse = response.data;

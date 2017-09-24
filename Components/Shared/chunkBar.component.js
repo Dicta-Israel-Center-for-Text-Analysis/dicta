e26bcba866d573fdb8563e1134fd085fb61a1f7a
Aryeh Sanders <aryehsanders@gmail.com>
2017-09-24 16:39:00 +0300

    fix chunkbar calculation: need more space between bars in this design

--- Components/Shared/chunkBar.component.js
+++ Components/Shared/chunkBar.component.js
@@ -12,8 +12,12 @@ jTextMinerApp.component('chunkBar',
                 const total = _.sumBy(ctrl.chunks, chunk => chunk.text.length);
                 // doesn't recalculate on resize
                 const viewHeight = document.documentElement.clientHeight;
+                // after removing the minimum size of each bar, how much extra space is there to allocate?
                 const extraHeight = viewHeight - 1 - ctrl.chunks.length*9;
-                return (extraHeight * (chunk.text.length / total) + 9)*100/viewHeight;
+                // this chunk should be extraHeight times its percentage of the text, plus a minimum 7 pixels,
+                // with an additional 3px for spacing between bars
+                // converted to vh units so that it will scale reasonably
+                return (extraHeight * (chunk.text.length / total) + 6)*100/viewHeight;
             };
 
             // ctrl.createThumbnail = function (chunk) {

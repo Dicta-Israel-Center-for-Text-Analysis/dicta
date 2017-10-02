f7631e3050d47f31896d65b298e7da79a642782c
Aryeh Sanders <aryehsanders@gmail.com>
2017-10-02 21:54:05 +0300

    comment out additional menu items that aren't working in search

--- Services/bibleContextMenu.service.js
+++ Services/bibleContextMenu.service.js
@@ -9,19 +9,19 @@ angular.module('JTextMinerApp')
                 [(scope, event) => "Search for " + event.target.innerText + " used in the same sense", function ($itemScope, $event, modelValue, text, $li) {
                     onSearch({query: $event.target.innerText, item: $itemScope});
                 }],
-                [(scope, event) => "Search for words with the same morphology as " + scope.word, function ($itemScope, $event, modelValue, text, $li) {
-                    onSearch({query: $event.target.innerText});
-                }],
-                null, // Divider
-                [(scope, event) => "Search for " /*+ scope.line.smallUnit.text*/, function ($itemScope, $event, modelValue, text, $li) {
-                    onSearch({query: $event.target.innerText});
-                }],
-                [(scope, event) => "Search for similar clauses", function ($itemScope, $event, modelValue, text, $li) {
-                    onSearch({query: $event.target.innerText});
-                }],
-                [(scope, event) => "Search for similar verses", function ($itemScope, $event, modelValue, text, $li) {
-                    onSearch({query: $event.target.innerText});
-                }]
+                // [(scope, event) => "Search for words with the same morphology as " + scope.word, function ($itemScope, $event, modelValue, text, $li) {
+                //     onSearch({query: $event.target.innerText});
+                // }],
+                // null, // Divider
+                // [(scope, event) => "Search for " /*+ scope.line.smallUnit.text*/, function ($itemScope, $event, modelValue, text, $li) {
+                //     onSearch({query: $event.target.innerText});
+                // }],
+                // [(scope, event) => "Search for similar clauses", function ($itemScope, $event, modelValue, text, $li) {
+                //     onSearch({query: $event.target.innerText});
+                // }],
+                // [(scope, event) => "Search for similar verses", function ($itemScope, $event, modelValue, text, $li) {
+                //     onSearch({query: $event.target.innerText});
+                // }]
             ]}
         };
     });

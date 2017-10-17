b114e2790138acfe4c3ed237bbabb628e678bba8
Aryeh Sanders <aryehsanders@gmail.com>
2017-10-17 16:39:59 +0300

    add link to show advanced per-chunk data in classification results

---
+++ Components/Classification/textClassificationDataDialog.component.js
@@ -0,0 +1,34 @@
+jTextMinerApp.component('textClassificationDataDialog',
+{
+    bindings: {
+        experiment: '<',
+        testFile: '<',
+        onConfirm: '&'
+    },
+    templateUrl: 'Components/Classification/textClassificationDataDialog.component.html',
+    controller: function() {
+        const ctrl = this;
+        
+        ctrl.convertFeatureName = function (featureName) {
+            let converted = featureName;
+            if (converted.indexOf('_') > -1) {
+                converted = prettyPrintMorphology(converted);
+            }
+            return converted;
+        };
+
+        ctrl.classes = ctrl.experiment.classes.Corpus_classes;
+
+        ctrl.indexOfClass = function (name) {
+            return ctrl.classes.map(classData => classData.title).indexOf(name)
+        };
+
+        ctrl.classIndexToColor = function (classNum) {
+            return ["#634C9F", "#FFD933", "#4EB6A3", "#B65A4E"][classNum];
+        }
+
+        ctrl.classNameToColor = function (className) {
+            return ctrl.classIndexToColor(ctrl.indexOfClass(className));
+        }
+    }
+}); 
--- Components/Classification/textWithFeatures.component.js
+++ Components/Classification/textWithFeatures.component.js
@@ -1,14 +1,23 @@
-﻿jTextMinerApp.component('textWithFeatures', {
+jTextMinerApp.component('textWithFeatures', {
     bindings: {
+        experiment: '<',
         testFile: '<',
         index: '<',
         classifiedClassIndex: '<',
         features: '<'
     },
     templateUrl: 'Components/Classification/textWithFeatures.component.html',
-    controller: ['$scope', '$sce', function ($scope, $sce) {
+    controller: function ($scope, $sce, DialogService) {
+        const ctrl = this;
         $scope.convert2TrustAsHtml = function (text) {
             return $sce.trustAsHtml(text);
         };
-    }]
+
+        ctrl.viewAdvanced = function () {
+            DialogService.openDialog('textClassificationDataDialog', {
+                experiment: ctrl.experiment,
+                testFile: ctrl.testFile
+            })
+        }
+    }
 });
